c91368601a67c75fb5f59603f5f906765d8ecd0e
Gençay Yıldız <gyildizmail@gmail.com>
2023-01-30 01:23:15 +0300

    RabbitMQ Eğitimi #4 - Basit Düzeyde Publisher ve Consumer Uygulamaları Oluşturma Detayları

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://befjdvjy:TbpYTaE6qqoRmL0ftZ3D2u5CiXvnuMPe@moose.rmq.cloudamqp.com/befjdvjy");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue'ya Mesaj Gönderme
+
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları bizim byte dönüşmemiz gerekecektir.
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
